0e963af8c1e475392eda8d0e31ac4ed24a00a43a
Sam Nichols <sjnichols89@gmail.com>
2019-10-23 15:39:21 -0700

    finished

--- week1app.java
+++ week1app.java
@@ -9,7 +9,7 @@ public class week1app {
 				String lastName1 = "Nichols";
 				String middleName1 = "Jacob";
 				int age1 = 30;
-				int friends1 = 10;
+				int friends1 = 30;
 				double moneyInWallet1 = 75.00;
 				double hotDogItemPrice1 = 3.50;
 				
@@ -22,6 +22,7 @@ public class week1app {
 				double moneyInWallet2 = 00.00;
 				double beerItemPrice2 = 8.00;
 				
+				
 				/*
 				 * transaction for purchase of and Item
 				 * Full Name
@@ -29,6 +30,9 @@ public class week1app {
 				 */
 
 				System.out.println("My name is " + firstName1 + " " + middleName1 + " " + lastName1);
+				System.out.println("After spending $" + beerItemPrice2 + " on 1 beer I have $" + (moneyInWallet1 - beerItemPrice2) + " left.");
+				System.out.println("Over " + age1 + " years I've made " + friends1 + " friends. That's " + (friends1 / age1) + " per year." );
+				
 	}
 
 }
