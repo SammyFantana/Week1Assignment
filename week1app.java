67923ae380d95197eda1ec64ffc78b044650ab8d
Sam Nichols <sjnichols89@gmail.com>
2019-10-23 15:22:56 -0700

    first commit

---
+++ week1app.java
@@ -0,0 +1,34 @@
+
+public class week1app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Variables for Week 1 project
+				//person one:
+				String firstName1 = "Sam";
+				String lastName1 = "Nichols";
+				String middleName1 = "Jacob";
+				int age1 = 30;
+				int friends1 = 10;
+				double moneyInWallet1 = 75.00;
+				double hotDogItemPrice1 = 3.50;
+				
+				//person two:
+				String firstName2 = "Momma";
+				String lastName2 = "Nichols";
+				String middleName2 = "Cat";
+				int age2 = 5;
+				int friends2 = 1;
+				double moneyInWallet2 = 00.00;
+				double beerItemPrice2 = 8.00;
+				
+				/*
+				 * transaction for purchase of and Item
+				 * Full Name
+				 * Amount of friends per a year.
+				 */
+
+				System.out.println("My name is " + firstName1 + " " + middleName1 + " " + lastName1);
+	}
+
+}
